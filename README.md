2176839c999b9d5af60c5bd7c79f57148c21ac0e
quake4u <47490815+quake4u@users.noreply.github.com>
2020-10-17 10:02:56 +0000

    simple chat

---
+++ README.md
@@ -0,0 +1,10 @@
+# coursera-cyber-capstone
+Cybersecurity capstone project - Fall 2020
+
+html folder contains all the files for the apache server.
+
+You have to change the Database.php (getDatabase function) file inside system folder to your database credentials.
+
+dbscript file is used to create your database.
+
+Tiago Pinho 2020
---
+++ html/system/Database.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace System\Database;
+
+use \PDO;
+use \DateTime;
+
+class Database
+{
+
+    function __construct()
+    {
+
+        if (!isset($GLOBALS['database_connection'])) {
+
+            $db_details = $this->getDatabase();
+
+            $serverName = $db_details['host'];
+            $username = $db_details['user'];
+            $password = $db_details['password'];
+            $databaseName = $db_details['database'];
+
+            $pdo = new PDO("mysql:host=$serverName;dbname=$databaseName", $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $GLOBALS['database_connection'] = $pdo;
+        }
+    }
+
+    function getDatabase()
+    {
+        /*
+        return array(
+            'host' => 'localhost',
+            'user' => 'capstone',
+            'password' => '4Eex0Y8S6Wusl!',
+            'database' => 'capstone'
+        );*/
+
+
+        return array(
+            'host' => getenv('DATABASE_HOST'),
+            'user' => getenv('DATABASE_USER'),
+            'password' => getenv('DATABASE_PASSWORD'),
+            'database' => getenv('DATABASE_NAME')
+        );
+    }
+
+    function getConnection()
+    {
+        return $GLOBALS['database_connection'];
+    }
+
+    function getFetchNum()
+    {
+        return PDO::FETCH_NUM;
+    }
+
+    function convertToDateTimeString(DateTime $datetime): string
+    {
+        return $datetime->format('Y-m-d H:i:s');
+    }
+
+    function convertFromDateTimeString($datetime_string): DateTime
+    {
+
+        return date_create_from_format('Y-m-d H:i:s', $datetime_string);
+    }
+}
